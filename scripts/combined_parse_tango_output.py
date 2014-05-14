edce3a0f09793f5c8a233193304c14b3bf423d8a
Lorenzo Riano <lorenzo.riano@gmail.com>
2014-05-14 11:32:22 -0700

    using the new parameters for the combined script

--- scripts/combined_parse_tango_output.py
+++ scripts/combined_parse_tango_output.py
@@ -1,42 +1,60 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-import sys
+import argparse
 from subprocess import call
 import os
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: {} /location/to/files outputbag.bag".format(sys.argv[0]))
-        print("The folder must be of the kind produced by Tango mapper. "
-              "The superframes subfolder should be there too."
-              )
-        return 1
 
-    folder_name = sys.argv[1]
-    output_bag =sys.argv[2]
+    parser = argparse.ArgumentParser(description="Runs a series of scripts and "
+                                                 "programs to parse the Tango "
+                                                 "output. The /tmp folder will "
+                                                 "be used for temporary bags.")
+    parser.add_argument('-d', '--directory',
+                        help="The folder must be of the kind produced by Tango "
+                             "mapper. The superframes subfolder should be there "
+                             "too. The folder must have images.txt, "
+                             "depth_to_imu_transformation.txt, "
+                             "fisheye_to_imu_transformation.txt, "
+                             "narrow_to_imu_transformation.txt",
+                        required=True)
+    parser.add_argument('-o', '--output',
+                        help='output bag file (with location)',
+                        metavar='bag_filename',
+                        type=str,
+                        required=True
+                        )
+
+
+    arguments = parser.parse_args()
+    folder_name = arguments.directory
+    output_bag = arguments.output
 
     #parse image file
     print("Parsing the images.txt file")
     call(['rosrun',
           'superframe_parser',
           'parse_images_file.py',
-          os.path.join(folder_name, 'images.txt'),
-          '/tmp/tango_poses.bag'])
+          '-i', os.path.join(folder_name, 'images.txt'),
+          '-o', '/tmp/tango_poses.bag',
+          '-y', 'True'
+         ])
 
     if not os.path.exists('/tmp/tango_poses.bag'):
         print("The bag /tmp/tango_poses.bag doesn't seem to exist, "
               "there probably was a problem in the previous run")
         return 1
 
-    #parse image file
+    #parse transformation file
     print("Parsing the transformation files")
     call(['rosrun',
           'superframe_parser',
           'parse_transformation_files.py',
-          folder_name,
-          '/tmp/tango_tfs.bag'])
+          '-d', folder_name,
+          '-o', '/tmp/tango_tfs.bag',
+          '-y', 'True'])
 
     if not os.path.exists('/tmp/tango_tfs.bag'):
         print("The bag /tmp/tango_tfs.bag doesn't seem to exist, "
@@ -49,7 +67,8 @@ def main():
           'superframe_parser',
           'parse_all_frames',
           folder_name,
-          '/tmp/tango_superframes.bag'])
+          '/tmp/tango_superframes.bag',
+          '-y'])
 
     if not os.path.exists('/tmp/tango_superframes.bag'):
         print("The bag /tmp/tango_superframes.bag doesn't seem to exist, "
@@ -61,10 +80,10 @@ def main():
     call(['rosrun',
           'superframe_parser',
           'merge_bags.py',
-          output_bag,
           '/tmp/tango_poses.bag',
           '/tmp/tango_tfs.bag',
-          '/tmp/tango_superframes.bag'])
+          '/tmp/tango_superframes.bag',
+          '-o', output_bag])
 
     if not os.path.exists(output_bag):
         print("The bag " + output_bag + " doesn't seem to exist, "
